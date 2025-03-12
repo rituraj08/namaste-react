fea30492a10b236cddc16a6a3ec127dc06a2640a
Ritu Raj <ritu.raj1@walmart.com>
2025-03-12 23:17:57 +0530

    Episode-1

---
+++ App.js
@@ -0,0 +1,19 @@
+// const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+const parent = React.createElement("div", {id:"parent"}, 
+
+    React.createElement("div", {id:"child"},
+
+        React.createElement("h1", {id:"child2"}, "I am h1 tag")
+        
+    )
+
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
